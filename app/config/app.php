a19576730b5f7d6156fb40f2111db83041de3f61
breekru <breekru@gmail.com>
2025-12-19 15:15:39 -0600

    undo of Threshold test

--- app/config/app.php
+++ app/config/app.php
@@ -137,7 +137,7 @@ return [
         /*
          | Risk thresholds (must align with scoring scale)
          */
-        'high_threshold'     => 1,
+        'high_threshold'     => 70,
         'critical_threshold' => 90,
 
         /*
