1a1294400024a88bfa127942c3369515f60d2cff
breekru <breekru@gmail.com>
2025-12-22 14:05:00 -0600

    Update domain.php

--- domain.php
+++ domain.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/auth/guard.php';
 require_once __DIR__ . '/app/bootstrap.php';
+require_once __DIR__ . '/auth/guard.php';
 //require_once __DIR__ . '/partials/header.php';
 
 // -------------------------
